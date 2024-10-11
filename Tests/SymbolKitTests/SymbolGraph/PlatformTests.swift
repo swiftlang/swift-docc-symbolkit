4a81a8d9ce1faa61599684829fff01b9bd952901
Sofia Rodriguez <sofia_rodriguez@apple.com>
2024-10-11 09:56:53 +0100

    Remove `Unsupported OS` string from unknown platforms.
        This change removes the addition of the string `Unsupported OS:` to platform names that are not considered as known platforms. Known platforms are apple platforms + Linux.
    
        DocC is being used to document REST APIs and other codebases outside of the initial ecosystem use case. Having this string was makig Symbolkit incompatible for all this other documentation use cases.
    
    rdar://112033286

--- Tests/SymbolKitTests/SymbolGraph/PlatformTests.swift
+++ Tests/SymbolKitTests/SymbolGraph/PlatformTests.swift
@@ -1,7 +1,7 @@
 /*
  This source file is part of the Swift.org open source project
 
- Copyright (c) 2021 Apple Inc. and the Swift project authors
+ Copyright (c) 2021-2024 Apple Inc. and the Swift project authors
  Licensed under Apache License v2.0 with Runtime Library Exception
 
  See https://swift.org/LICENSE.txt for license information
@@ -35,15 +35,15 @@ class PlatformTests: XCTestCase {
         }
     }
 
-    func testInvalidOperatingSystemName() {
+    func testUnknownOperatingSystemName() {
         let platform = SymbolGraph.Platform(
             architecture: nil,
             vendor: nil,
-            operatingSystem: SymbolGraph.OperatingSystem(name: "invalidos"),
+            operatingSystem: SymbolGraph.OperatingSystem(name: "unknown"),
             environment: nil
         )
 
-        XCTAssertEqual(platform.name, "Unsupported OS: invalidos")
+        XCTAssertEqual(platform.name, "unknown")
     }
 
     func testMacCatalystName() {
