9e6065fd7e2ad0ca1611e586ac0679641d6309f5
Sofia Rodriguez <sofia_rodriguez@apple.com>
2024-10-11 09:44:36 +0100

    Cleanup macabi platform name logic

--- Sources/SymbolKit/SymbolGraph/Platform.swift
+++ Sources/SymbolKit/SymbolGraph/Platform.swift
@@ -49,13 +49,10 @@ extension SymbolGraph {
             switch os {
             case "macosx", "macos":
                 return SymbolGraph.Symbol.Availability.Domain.macOS
+            case "ios" where environment == "macabi":
+                return SymbolGraph.Symbol.Availability.Domain.macCatalyst
             case "ios":
-                if environment == "macabi" {
-                    return SymbolGraph.Symbol.Availability.Domain.macCatalyst
-
-                } else {
-                    return SymbolGraph.Symbol.Availability.Domain.iOS
-                }
+                return SymbolGraph.Symbol.Availability.Domain.iOS
             case "watchos":
                 return SymbolGraph.Symbol.Availability.Domain.watchOS
             case "tvos":
