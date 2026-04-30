5ae93a807f1e83c918a9b63506e10133ef4b14ed
Sofia <sofia_rodriguez@apple.com>
2026-04-30 16:27:13 +0100

    Define ARM 64-bit architecture names.
    Define a property that determines if a symbolgraph was generated for
    Apple Silicon given its platform architecture information.

--- Sources/SymbolKit/SymbolGraph/Platform.swift
+++ Sources/SymbolKit/SymbolGraph/Platform.swift
@@ -15,6 +15,9 @@ extension SymbolGraph {
          The name of the architecture that this module targets, such as `x86_64` or `arm64`. If the module doesn't have a specific architecture, this may be undefined.
          */
         public var architecture: String?
+        var isAppleSilicon: Bool {
+            architecture == "aarch64" || architecture == "arm64"
+        }
 
         /**
          The platform vendor from which this module came, such as `apple` or `linux`.
